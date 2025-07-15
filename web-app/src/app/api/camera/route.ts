d5675fd74d5da15a63c2700e16d2b77400afdaa6
THATONEKO <thatoneko03@gmail.com>
2025-07-15 10:50:49 +0200

    Temporary fix for /api/camera without DB

--- web-app/src/app/api/camera/route.ts
+++ web-app/src/app/api/camera/route.ts
@@ -1,6 +1,3 @@
-import { prisma } from "@/lib/db/prisma";
-// import updateAndCacheRouteStats from "@/lib/db/updateSpeed";
-import { redis } from "@/lib/db/redis";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
@@ -11,21 +8,12 @@ export const POST = async (req: NextRequest) => {
       return NextResponse.json({ error: "Missing time" }, { status: 400 });
     }
 
-    const latestDoc = await prisma.sensor1.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
+    console.log("🚗 Vehicle detected at:", time);
 
-    if (!latestDoc) {
-      return NextResponse.json({ message: "No data found" }, { status: 404 });
-    }
-
-    await redis.set(`sensor1`, JSON.stringify(latestDoc.speed));
-
-    return NextResponse.json({
-      speed: latestDoc.speed,
-    });
+    return NextResponse.json({ message: "Vehicle detection received successfully." });
   } catch (err) {
-    console.error(err);
+    console.error("[ERROR] /api/camera crash:", err);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 };
+
